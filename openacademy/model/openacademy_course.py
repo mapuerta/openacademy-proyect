ec5f549121f2ad2844e720ba6666480c055a7ac5
Mauricio Sanchez <mauricio@vauxoo.com>
2016-02-04 19:05:50 +0000

    [REF] openacademy: Adding m2o fields

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -10,4 +10,6 @@ class Course(models.Model):
      _name = 'openacademy.course'
      name = fields.Char(string="Title", required=True)
      description = fields.Text(string="Description")
-
+     responsible_id = fields.Many2one('res.users',
+                     		     ondelete='set null',
+				     string="Responsible", index=True)
