1eeadfaf20de9791ba44d3cc55db8b769a0b3bcc
Mauricio Sanchez <mauricio@vauxoo.com>
2016-02-05 21:56:01 +0000

    [REF] openacademy: Adding graph view

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -1,5 +1,5 @@
-
 from datetime import timedelta
+
 from openerp import api, exceptions, fields, models
 
 class Session(models.Model):
@@ -13,18 +13,20 @@ class Session(models.Model):
                                    domain=['|',
 					("instructor", "=", True),
 					("category_id.name", "ilike", "Teacher"),
-				   ])   
-    
+				   ])
    course_id = fields.Many2one('openacademy.course',
-         ondelete='cascade', string="Course", required=True)
+        ondelete='cascade', string="Course", required=True)
    attendee_ids = fields.Many2many('res.partner', string="Attendees")
-   taken_seats = fields.Float(string="Taken seats",  compute='_taken_seats',
+   taken_seats = fields.Float(string="Taken seats", compute='_taken_seats',
                               store=True)
    active = fields.Boolean(default=True)
    end_date = fields.Date(string="End Date", store=True,
-       compute='_get_end_date', inverse='_set_end_date')
+        compute='_get_end_date', inverse='_set_end_date')
    hours = fields.Float(string="Duration in hours",
                          compute='_get_hours', inverse='_set_hours')
+   attendees_count = fields.Integer(
+        string="Attendees count", compute='_get_attendees_count', store=True)
+
 
    @api.one
    @api.depends('seats', 'attendee_ids')
@@ -33,10 +35,10 @@ class Session(models.Model):
            self.taken_seats = 0
        else:
            self.taken_seats = 100.0 * len(self.attendee_ids) / self.seats
-   
+
    @api.onchange('seats', 'attendee_ids')
    def _verify_valid_seats(self):
-	if self.seats < 0:
+        if self.seats < 0:
             return {
                 'warning': {
                     'title': "Incorrect 'seats' value",
@@ -50,14 +52,13 @@ class Session(models.Model):
                     'message': "Increase seats or remove excess attendees",
                 },
             }
-   
+
    @api.one
    @api.constrains('instructor_id', 'attendee_ids')
    def _check_instructor_not_in_attendees(self):
        if self.instructor_id and self.instructor_id in self.attendee_ids:
             raise exceptions.ValidationError("A session's instructor can't be an attendee")
-   
-     
+
    @api.one
    @api.depends('duration', 'start_date')
    def _get_end_date(self):
@@ -70,7 +71,7 @@ class Session(models.Model):
         start = fields.Datetime.from_string(self.start_date)
         duration = timedelta(days=self.duration, seconds=-1)
         self.end_date = start + duration
-    
+
    @api.one
    def _set_end_date(self):
         if not (self.start_date and self.end_date):
@@ -81,7 +82,7 @@ class Session(models.Model):
         start_date = fields.Datetime.from_string(self.start_date)
         end_date = fields.Datetime.from_string(self.end_date)
         self.duration = (end_date - start_date).days + 1
-    
+
    @api.one
    @api.depends('duration')
    def _get_hours(self):
@@ -90,3 +91,8 @@ class Session(models.Model):
    @api.one
    def _set_hours(self):
         self.duration = self.hours / 24
+
+   @api.one
+   @api.depends('attendee_ids')
+   def _get_attendees_count(self):
+      self.attendees_count = len(self.attendee_ids)
