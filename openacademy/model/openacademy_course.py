4313c5528efd02c45013907300b33436ce31eddd
Mauricio Sanchez <mauricio@vauxoo.com>
2016-02-05 20:28:24 +0000

    [REF] openacademy: Modify copy method into inherit

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -19,8 +19,23 @@ class Course(models.Model):
         ('name_description_check',
          'CHECK(name != description)',
          "The title of the course should not be the description"),
-
+     
         ('name_unique',
          'UNIQUE(name)',
          "The course title must be unique"),
-    ]
+     ]
+     
+     @api.one  #api.one send defaults params: cr, uid, id, context
+     def copy(self, default=None):
+        print "estoy pasando por la funcion heredada de copy en cursos"
+        #  default['name'] = self.name + ' (copy)'
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+        
+        default['name'] = new_name
+        return super(Course, self).copy(default)
